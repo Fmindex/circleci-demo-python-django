6d8c4318fe628adbe0fdc7ed125a4bf5c23d81cc
FM <fm@FMs-MacBook-Pro.local>
2018-11-10 21:20:48 +0700

    make failed

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,7 @@ def index(request):
     View function for home page of site.
     """
     # Generate counts of some of the main objects
-#    num_books=Book.objects.all().count()
+    num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     # Available copies of books
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
@@ -43,12 +43,12 @@ class BookDetailView(generic.DetailView):
     """
     model = Book
 
-class AuthorListView(generic.ListView):
+#class AuthorListView(generic.ListView):
     """
     Generic class-based list view for a list of authors.
     """
-    model = Author
-    paginate_by = 10 
+ #   model = Author
+  #  paginate_by = 10 
 
 
 class AuthorDetailView(generic.DetailView):
